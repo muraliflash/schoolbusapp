cc214811dce30b813e12dca6dcb23f0efba3ee88
IOSLevel-01 <muraliflash12@gmail.com>
2019-01-29 11:12:10 +0530

    Management app layout

---
+++ schoolbus/schoolbus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  schoolbus
+//
+//  Created by IOSLevel-01 on 29/01/19.
+//  Copyright © 2019 SJBIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
